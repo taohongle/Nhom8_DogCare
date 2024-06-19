dcee5789619731632b9a97ae20d35c302c0854b7
Thuongdo823 <thuongdo2k3@gmail.com>
2024-06-19 22:47:27 +0700

    up

--- DogCareApp/app/src/main/java/tlu/cse/android/ht63/dogcareapp/fragment/HomeFragment.java
+++ DogCareApp/app/src/main/java/tlu/cse/android/ht63/dogcareapp/fragment/HomeFragment.java
@@ -43,7 +43,7 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // ... 
+        // ...
 
         initBanner();
     }
